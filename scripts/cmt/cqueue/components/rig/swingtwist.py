8372dfee76ed4eac35dbeb1ee6dc0e3f7346eb45
Chad Vernon <chadvernon@gmail.com>
2016-03-14 23:41:54 -0700

    Add swingtwist component.

---
+++ scripts/cmt/cqueue/components/rig/swingtwist.py
@@ -0,0 +1,130 @@
+import maya.cmds as cmds
+from PySide import QtGui
+import cmt.cqueue.core as core
+import cmt.cqueue.fields as fields
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Component(core.Component):
+    """A Component that creates swingTwist nodes."""
+    twist_axis = {
+        0: 'X',
+        1: 'Y',
+        2: 'Z',
+    }
+
+    def __init__(self, swingtwists=None, **kwargs):
+        """Constructor
+        :param swingtwists: A list of dictionaries describing the swingTwist nodes that need to be
+                           created:
+            {
+                'driven': node,
+                'driver': nodes,
+                'name': name,
+                'twist': 1.0,
+                'swing': 0.2,
+                'invertTwist': False,
+                'invertSwing': False,
+            }
+        """
+        super(Component, self).__init__(**kwargs)
+        self.swingtwists = fields.ArrayField(add_label_text='Add SwingTwist')
+        if not swingtwists:
+            # Create default entries if none specified
+            swingtwists = [
+                {'name': 'swingTwist#'}
+            ]
+        twist_axes = self.twist_axis.values()
+        twist_axes.sort()
+        # The fields will be arranged in two row containers
+        # [[driver, driven], [name, twist, swing, invertTwist, invertSwing, twistAxis]]
+        for swingtwist in swingtwists:
+            container = fields.ContainerField(orientation=fields.ContainerField.vertical)
+            self.swingtwists.add_field(container)
+
+            row_container = fields.ContainerField(border=False)
+            container.add_field(row_container)
+            row_container.add_field(fields.MayaNodeField(name='Driver',
+                                                         value=swingtwist.get('driver', ''),
+                                                         help_text='The node to drive the swingtwist'))
+            row_container.add_field(fields.MayaNodeField(name='Driven',
+                                                         value=swingtwist.get('driven', ''),
+                                                         help_text='The node to be driven'))
+
+            row_container = fields.ContainerField(border=False)
+            container.add_field(row_container)
+            row_container.add_field(fields.CharField(
+                name='Name', value=swingtwist.get('name', 'swingTwist#'),
+                help_text='The name of the created swingTwist node.'))
+            row_container.add_field(fields.FloatField(name='Twist',
+                                                      value=swingtwist.get('twist', 1.0),
+                                                      help_text='The twist amount',
+                                                      max_value=1.0))
+            row_container.add_field(fields.FloatField(name='Swing',
+                                                      value=swingtwist.get('swing', 1.0),
+                                                      help_text='The swing amount',
+                                                      max_value=1.0))
+            row_container.add_field(fields.BooleanField(name='Invert Twist',
+                                                        value=swingtwist.get('invertTwist', False),
+                                                        help_text='Whether to invert the twist.'))
+            row_container.add_field(fields.BooleanField(name='Invert Swing',
+                                                        value=swingtwist.get('invertSwing', False),
+                                                        help_text='Whether to invert the swing.'))
+            row_container.add_field(fields.ChoiceField(
+                name='Twist Axis',
+                value=Component.twist_axis[swingtwist.get('twistAxis', 0)],
+                choices=twist_axes,
+                help_text='The twist axis'))
+
+    def execute(self):
+        cmds.loadPlugin('cmt_py', qt=True)
+        for container in self.swingtwists:
+            driver = container[0][0].value()
+            driven = container[0][1].value()
+            if not cmds.objExists(driver) or not cmds.objExists(driven):
+                logger.warning('{0} or {1} does not exist.'.format(driver, driven))
+                continue
+            name = container[1][0].value()
+            logger.info('Creating swingtwist on {0} from {1}'.format(driven, driver))
+            node = cmds.swingTwist(driver, driven, name=name)
+
+            attributes = get_swingtwist_attribute_dictionary(container)
+            for attr, value in attributes.items():
+                try:
+                    cmds.setAttr('{0}.{1}'.format(node, attr), value)
+                except RuntimeError:
+                    pass
+
+    def _data(self):
+        # Convert the fields back into a list of dictionaries
+        swingtwists = []
+        for container in self.swingtwists:
+            attributes = get_swingtwist_attribute_dictionary(container)
+            attributes['name'] = container[1][0].value()
+            attributes['driver'] = container[0][0].value()
+            attributes['driven'] = container[0][1].value()
+            swingtwists.append(attributes)
+        return {
+            'swingtwists': swingtwists
+        }
+
+    def draw(self, layout):
+        """Renders the component PySide widgets into the given layout."""
+        layout.addWidget(self.swingtwists.widget())
+
+
+def get_swingtwist_attribute_dictionary(container):
+    """Convenience function to turn a ContainerField into a dictionary holding all the attributes
+    of the swingTwist.
+
+    :param container: A ContainerField containing all the
+    :return:
+    """
+    return {
+        'twist': container[1][1].value(),
+        'swing': container[1][2].value(),
+        'invertTwist': container[1][3].value(),
+        'invertSwing': container[1][4].value(),
+        'twistAxis': 'XYZ'.index(container[1][5].value()),
+    }
