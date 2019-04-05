24e7731e06170f01d596d0e539af3562299c2832
Chad Vernon <chadvernon@gmail.com>
2019-04-04 23:14:51 -0700

    meshretarget works...but really slow

--- scripts/cmt/rig/meshretarget.py
+++ scripts/cmt/rig/meshretarget.py
@@ -1,61 +1,64 @@
+"""Retarget meshes fit on a source mesh to a modified version of the source mesh.
+
+Most of this was taken from http://mathlab.github.io/PyGeM/_modules/pygem/radial.html#RBF
+
+Example Usage
+=============
+
+    retarget("source_body", "new_body", ["shirt", "pants"], rbf=RBF.linear)
+
+"""
 import math
 import numpy as np
-from scipy.linalg import lu_factor, lu_solve, solve
+from scipy.spatial.distance import cdist
 
 import maya.api.OpenMaya as OpenMaya
 import maya.cmds as cmds
 import cmt.shortcuts as shortcuts
 
 
-def retarget(source, target, shapes, eps=1.0):
-    source_points = get_points(source)
-    target_points = get_points(target)
-    distances = [
-        [sp1.distanceTo(sp2) for sp1 in source_points] for sp2 in source_points
-    ]
-    max_distance = max([max(d) for d in distances])
+def retarget(source, target, shapes, rbf=None, radius=0.5, stride=1):
+    """Run the mesh retarget.
+
+    :param source: Source mesh
+    :param target: Modified source mesh
+    :param shapes: List of meshes to retarget
+    :param rbf: One of the RBF functions. See class RBF
+    :param radius: Smoothing parameter for the rbf
+    :param stride: Vertex stride to sample on the source mesh.  Increase to speed up
+    the calculation.
+    """
+    source_points = points_to_np_array(source, stride)
+    target_points = points_to_np_array(target, stride)
+
+    if rbf is None:
+        rbf = RBF.linear
+    # dist = get_distance_matrix(source_points, source_points, RBF.linear, 0)
+    # max_distance = np.amax(dist)
+    max_distance = 1.0
+    weights = get_weight_matrix(source_points, target_points, rbf, radius, max_distance)
 
     for shape in shapes:
-        points = get_points(shape)
-        for i in range(len(points)):
-            shape_distances = [points[i].distanceTo(sp) for sp in source_points]
-            max_d = max(shape_distances)
-            if max_d > max_distance:
-                max_distance = max_d
-
-    distances = [[rbf(d / max_distance, eps) for d in d1] for d1 in distances]
-    # print(distances)
-    # distances = [[d for d in d1] for d1 in distances]
-
-    a = np.array(distances)
-    # print("a = {}".format(a))
-
-    b = np.array([[tp.x, tp.y, tp.z] for tp in target_points])
-    # print("b = {}".format(b))
-
-    x = solve(a, b)
-    # print("x = {}".format(x))
-
-    for shape in shapes:
-        points = get_points(shape)
-        for i in range(len(points)):
-            distances = [
-                rbf(points[i].distanceTo(sp) / max_distance, eps)
-                for sp in source_points
-            ]
-            # print(distances)
-            d = np.array(distances)
-            result = np.matmul(d, x)
-            points[i] = OpenMaya.MPoint(*result)
-
-        dupe = cmds.duplicate(shape, name="{}_{}".format(shape, eps))[0]
+        points = points_to_np_array(shape)
+        n_points = points.shape[0]
+        dist = get_distance_matrix(points, source_points, rbf, radius, max_distance)
+        identity = np.ones((n_points, 1))
+        h = np.bmat([[dist, identity, points]])
+        # h = dist
+        deformed = np.asarray(np.dot(h, weights))
+        points = [OpenMaya.MPoint(*p) for p in deformed]
+        dupe = cmds.duplicate(
+            shape, name="{}_{}_{}".format(shape, radius, rbf.__name__)
+        )[0]
         set_points(dupe, points)
         cmds.delete(cmds.parentConstraint(target, dupe))
 
 
-def rbf(v, eps=1.0):
-    return v
-    # return math.exp(-(v / eps) ** 2)
+def points_to_np_array(mesh, stride=1):
+    points = get_points(mesh)
+    points = [p for p in points][::stride]
+    points = np.array([[p.x, p.y, p.z] for p in points])
+    return points
 
 
 def get_points(mesh):
@@ -64,7 +67,85 @@ def get_points(mesh):
     return mesh_fn.getPoints()
 
 
+def get_weight_matrix(sp, tp, rbf, radius, max_distance):
+    """Get the weight matrix x in Ax=B
+
+    :param sp: Source control point array
+    :param tp: Target control point aray
+    :param rbf: Rbf function from class RBF
+    :param radius: Smoothing parameter
+    :param max_distance: Max distance used to normalize distances
+
+    :return: Weight matrix
+    """
+    identity = np.ones((sp.shape[0], 1))
+    dist = get_distance_matrix(sp, sp, rbf, radius, max_distance)
+    # Solve x for Ax=B
+    dim = 3
+    a = np.bmat(
+        [
+            [dist, identity, sp],
+            [identity.T, np.zeros((1, 1)), np.zeros((1, dim))],
+            [sp.T, np.zeros((dim, 1)), np.zeros((dim, dim))],
+        ]
+    )
+    b = np.bmat([[tp], [np.zeros((1, dim))], [np.zeros((dim, dim))]])
+    weights = np.linalg.solve(a, b)
+    return weights
+
+
+def get_distance_matrix(v1, v2, rbf, radius, max_distance=1.0):
+    matrix = cdist(v1, v2, lambda x, y: rbf((x - y) / max_distance, radius))
+    return matrix
+
+
 def set_points(mesh, points):
     path = shortcuts.get_dag_path2(shortcuts.get_shape(mesh))
     mesh_fn = OpenMaya.MFnMesh(path)
     mesh_fn.setPoints(points)
+
+
+class RBF(object):
+    """Various RBF kernels"""
+
+    @classmethod
+    def linear(cls, vector, radius):
+        norm = np.linalg.norm(vector)
+        return norm
+
+    @classmethod
+    def gaussian(cls, vector, radius):
+        norm = np.linalg.norm(vector)
+        result = np.exp(-(norm * norm) / (radius * radius))
+        return result
+
+    @classmethod
+    def thin_plate(cls, vector, radius):
+        norm = np.linalg.norm(vector / radius)
+        result = norm * norm
+        if norm > 0:
+            result *= np.log(norm)
+        return result
+
+    @classmethod
+    def multi_quadratic_biharmonic(cls, vector, radius):
+        norm = np.linalg.norm(vector)
+        result = np.sqrt((norm * norm) + (radius * radius))
+        return result
+
+    @classmethod
+    def inv_multi_quadratic_biharmonic(cls, vector, radius):
+        norm = np.linalg.norm(vector)
+        result = 1.0 / (np.sqrt((norm * norm) + (radius * radius)))
+        return result
+
+    @classmethod
+    def beckert_wendland_c2_basis(cls, vector, radius):
+        norm = np.linalg.norm(vector)
+        arg = norm / radius
+        first = 0
+        if (1 - arg) > 0:
+            first = np.power((1 - arg), 4)
+        second = (4 * arg) + 1
+        result = first * second
+        return result
