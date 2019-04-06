Fix vertex[0] bug.  Speed up calculation significantly.
For some reason reusing the same variable name as the result of a list comprehension on itself (for the vertex stride) was messing up the data.  Simply renaming to unique names fixed it.

Updated cdist to not run independently on each element.  Used the built-in euclidean distance and batch calls to take advantage of the vectorization.

scripts/cmt/rig/meshretarget.py
@@ -9,6 +9,7 @@ Example Usage
 
 """
 import math
+import time
 import numpy as np
 from scipy.spatial.distance import cdist
 
@@ -26,39 +27,38 @@ def retarget(source, target, shapes, rbf=None, radius=0.5, stride=1):
     :param rbf: One of the RBF functions. See class RBF
     :param radius: Smoothing parameter for the rbf
     :param stride: Vertex stride to sample on the source mesh.  Increase to speed up
-    the calculation.
+    the calculation but less accurate.
     """
+    start_time = time.time()
     source_points = points_to_np_array(source, stride)
     target_points = points_to_np_array(target, stride)
 
     if rbf is None:
         rbf = RBF.linear
-    # dist = get_distance_matrix(source_points, source_points, RBF.linear, 0)
-    # max_distance = np.amax(dist)
-    max_distance = 1.0
-    weights = get_weight_matrix(source_points, target_points, rbf, radius, max_distance)
+    weights = get_weight_matrix(source_points, target_points, rbf, radius)
 
     for shape in shapes:
         points = points_to_np_array(shape)
         n_points = points.shape[0]
-        dist = get_distance_matrix(points, source_points, rbf, radius, max_distance)
+        dist = get_distance_matrix(points, source_points, rbf, radius)
         identity = np.ones((n_points, 1))
         h = np.bmat([[dist, identity, points]])
-        # h = dist
         deformed = np.asarray(np.dot(h, weights))
         points = [OpenMaya.MPoint(*p) for p in deformed]
         dupe = cmds.duplicate(
             shape, name="{}_{}_{}".format(shape, radius, rbf.__name__)
         )[0]
         set_points(dupe, points)
-        cmds.delete(cmds.parentConstraint(target, dupe))
+
+    end_time = time.time()
+    print("Transferred in {} seconds".format(end_time - start_time))
 
 
 def points_to_np_array(mesh, stride=1):
     points = get_points(mesh)
-    points = [p for p in points][::stride]
-    points = np.array([[p.x, p.y, p.z] for p in points])
-    return points
+    sparse_points = [p for p in points][::stride]
+    np_points = np.array([[p.x, p.y, p.z] for p in sparse_points])
+    return np_points
 
 
 def get_points(mesh):
@@ -67,19 +67,18 @@ def get_points(mesh):
     return mesh_fn.getPoints()
 
 
-def get_weight_matrix(sp, tp, rbf, radius, max_distance):
+def get_weight_matrix(sp, tp, rbf, radius):
     """Get the weight matrix x in Ax=B
 
     :param sp: Source control point array
     :param tp: Target control point aray
     :param rbf: Rbf function from class RBF
     :param radius: Smoothing parameter
-    :param max_distance: Max distance used to normalize distances
 
     :return: Weight matrix
     """
     identity = np.ones((sp.shape[0], 1))
-    dist = get_distance_matrix(sp, sp, rbf, radius, max_distance)
+    dist = get_distance_matrix(sp, sp, rbf, radius)
     # Solve x for Ax=B
     dim = 3
     a = np.bmat(
@@ -94,8 +93,10 @@ def get_weight_matrix(sp, tp, rbf, radius, max_distance):
     return weights
 
 
-def get_distance_matrix(v1, v2, rbf, radius, max_distance=1.0):
-    matrix = cdist(v1, v2, lambda x, y: rbf((x - y) / max_distance, radius))
+def get_distance_matrix(v1, v2, rbf, radius):
+    matrix = cdist(v1, v2, "euclidean")
+    if rbf != RBF.linear:
+        matrix = rbf(matrix, radius)
     return matrix
 
 
@@ -109,43 +110,40 @@ class RBF(object):
     """Various RBF kernels"""
 
     @classmethod
-    def linear(cls, vector, radius):
-        norm = np.linalg.norm(vector)
-        return norm
+    def linear(cls, matrix, radius):
+        return matrix
 
     @classmethod
-    def gaussian(cls, vector, radius):
-        norm = np.linalg.norm(vector)
-        result = np.exp(-(norm * norm) / (radius * radius))
+    def gaussian(cls, matrix, radius):
+        result = np.exp(-(matrix * matrix) / (radius * radius))
         return result
 
     @classmethod
-    def thin_plate(cls, vector, radius):
-        norm = np.linalg.norm(vector / radius)
-        result = norm * norm
-        if norm > 0:
-            result *= np.log(norm)
+    def thin_plate(cls, matrix, radius):
+        result = matrix / radius
+        result *= matrix
+
+        np.warnings.filterwarnings("ignore")
+        result = np.where(result > 0, np.log(result), result)
+        np.warnings.filterwarnings("always")
+
         return result
 
     @classmethod
-    def multi_quadratic_biharmonic(cls, vector, radius):
-        norm = np.linalg.norm(vector)
-        result = np.sqrt((norm * norm) + (radius * radius))
+    def multi_quadratic_biharmonic(cls, matrix, radius):
+        result = np.sqrt((matrix * matrix) + (radius * radius))
         return result
 
     @classmethod
-    def inv_multi_quadratic_biharmonic(cls, vector, radius):
-        norm = np.linalg.norm(vector)
-        result = 1.0 / (np.sqrt((norm * norm) + (radius * radius)))
+    def inv_multi_quadratic_biharmonic(cls, matrix, radius):
+        result = 1.0 / (np.sqrt((matrix * matrix) + (radius * radius)))
         return result
 
     @classmethod
-    def beckert_wendland_c2_basis(cls, vector, radius):
-        norm = np.linalg.norm(vector)
-        arg = norm / radius
-        first = 0
-        if (1 - arg) > 0:
-            first = np.power((1 - arg), 4)
+    def beckert_wendland_c2_basis(cls, matrix, radius):
+        arg = matrix / radius
+        first = np.zeros(matrix.shape)
+        first = np.where(1 - arg > 0, np.power(1 - arg, 4), first)
         second = (4 * arg) + 1
         result = first * second
         return result